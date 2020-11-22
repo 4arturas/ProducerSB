5aec28de3bdad9266cb46ce1945acaeb569e89a7
Art <you@example.com>
2020-11-22 09:54:47 +0200

    Update ProduceController.java

--- src/main/java/com/producer/controllers/ProduceController.java
+++ src/main/java/com/producer/controllers/ProduceController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +21,40 @@ public class ProduceController
     @Value( value = "${kafka.topic.example-topic}" )
     private String topicExample;
 
-    void sendMesage( final String msg )
+    void sendMesageSimpleIplementation( final String msg )
     {
         kafkaTemplate.send( topicExample, msg );
+    }
+
+    void sendMessage( final String message )
+    {
+
+        final String topicName = topicExample;
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(topicName, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>()
+        {
+
+            @Override
+            public void onSuccess( SendResult<String, String> result )
+            {
+                final String info = String.format("Sent message=[%s] with offset=[%s]", message, result.getRecordMetadata().offset() );
+                log.info( info );
+            }
+            @Override
+            public void onFailure( Throwable ex )
+            {
+                final String info = String.format( "Unable to send message=[%s] due to : %s", message, ex.getMessage() );
+                log.info( info );
+            }
+        });
     }
 
     @GetMapping("/produce/{msg}")
     public String produce( @PathVariable String msg )
     {
-        sendMesage( msg );
+        sendMesageSimpleIplementation( msg );
         return msg;
     }
 }
